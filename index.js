88bbc2a91ef55e7e5a8a8f2dfd0879262f59fd5b
sifat786 <solaimanislamsifat@gmail.com>
2024-05-12 14:57:56 +0600

    create get method for blogs and specific blog

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,8 +15,49 @@ app.use(cors({
 app.use(express.json());
 
 
+// ///TODO: MondoDB:
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zzvfjhd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const blogCollection = client.db("freshThoughts").collection("blogs");
+
+    //**  **/
+    //! GET
+    app.get('/blogs', async(req, res) => {
+        const result = await blogCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/blogs/:id', async(req, res) => {
+        const query = { _id: new ObjectId(req.params.id) };
+        const result = await blogCollection.findOne(query);
+        res.send(result);
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.use('/', (req, res) => {
-    res.send('server is running');
+    res.send('blog is running');
 })
 
 app.listen(port, () => {
