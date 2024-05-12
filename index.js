7b8d02c260ef0bf7612ff692274350ba0968f63d
sifat786 <solaimanislamsifat@gmail.com>
2024-05-12 13:36:32 +0600

    added middle ware and cors setup

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //* Middleware:
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
+app.use(express.json());
+
+
 app.use('/', (req, res) => {
     res.send('server is running');
 })
